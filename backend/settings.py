f3475890df4130b94b38fff37bfdac7bac338202
Angel <apineross@unal.edu.co>
2024-12-28 13:28:27 -0500

    Enable CORS for all origins

--- backend/settings.py
+++ backend/settings.py
@@ -13,3 +13,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # URL del frontend local
     "https://maki-shop.up.railway.app",  # URL del frontend desplegado
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
