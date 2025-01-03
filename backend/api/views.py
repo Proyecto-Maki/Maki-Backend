7bd4953ba15812faa02ddb5c5755d898ffbaec26
kellysolanomt <ksolanoc@unal.edu.co>
2025-01-03 17:34:09 -0500

    cambio 2

--- backend/api/views.py
+++ backend/api/views.py
@@ -23,7 +23,7 @@ from rest_framework.decorators import api_view
 def SendTestEmail(request):
     try: 
         response = send_test_email()
-        return response
+        return response.error
     except Exception as e:
         return Response({
             'error': str(e.body),
