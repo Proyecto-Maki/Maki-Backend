e9996a1d7080dce584d3ea20f0414c566acae47e
kellysolanomt <ksolanoc@unal.edu.co>
2025-01-02 21:05:00 -0500

    Mascotas en admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Cliente, Fundacion
+from .models import User, Cliente, Fundacion, Mascota
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Cliente)
 admin.site.register(Fundacion)
+admin.site.register(Mascota)
+
