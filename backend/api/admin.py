7630c8371d321cd2aa3f68e4eb90354c053d4144
Kelly Solano <kelly.solano.1403@gmail.com>
2025-01-28 08:23:22 -0500

    Agregada a admin de categorias

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -16,7 +16,10 @@ from .models import (
     Direccion,
     PublicacionAdopcion,
     DetalleMascota,
-    SolicitudAdopcion
+    SolicitudAdopcion,
+    CategoriaPrincipal,
+    Categoria,
+    Subcategoria,
 )
 
 # Register your models here.
@@ -36,6 +39,10 @@ admin.site.register(Direccion)
 admin.site.register(PublicacionAdopcion)
 admin.site.register(DetalleMascota)
 admin.site.register(SolicitudAdopcion)
+admin.site.register(CategoriaPrincipal)
+admin.site.register(Categoria)
+admin.site.register(Subcategoria)
+
 
 
 
