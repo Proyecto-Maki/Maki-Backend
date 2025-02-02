998486f9c255ecd2d4d07dc1e4b12c202f5ad176
kellysolanomt <ksolanoc@unal.edu.co>
2025-02-02 17:41:44 -0500

    Intento de traer errores de sendgrid

--- backend/api/views.py
+++ backend/api/views.py
@@ -247,7 +247,16 @@ class ClienteSignupView(generics.ListCreateAPIView):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            send_code_to_user(user.email)
+            sendgrid_response = self.send_code_to_user(user.email)
+            if sendgrid_response.status_code != 200:
+                user.delete()
+                return Response(
+                    {
+                        "error": sendgrid_response.text,
+                        "detail": "Ha ocurrido un error en el envió de tu correo de confirmación. Comunicamente con soporte técnico.",
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
             return Response(
                 {
                     "user": UserSerializer(
